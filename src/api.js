37796cd739a4247392a2d0f9e836c19a90515bca
Andre Herter <andre-herter@web.de>
2025-04-18 20:40:45 +0200

    Fix api_url

--- src/api.js
+++ src/api.js
@@ -1,4 +1,4 @@
-const API_BASE_URL = "http://api.weatherapi.com/v1";
+const API_BASE_URL = "https://api.weatherapi.com/v1";
 const API_KEY = "9226203f0d6c46ea9bc85200252603";
 
 export async function getForecastWeather(location, days = 3) {
--- src/main.js
+++ src/main.js
@@ -3,4 +3,4 @@ import { loadCurrentWeather } from "./currentWeather";
 
 export const rootElement = document.getElementById("app");
 
-loadCurrentWeather("London");
+loadCurrentWeather("Kreuztal");
